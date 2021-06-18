fc9db17ac77eabae549153c4f6a368e6372b6cca
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-18 13:15:29 +0200

    getUsername upload

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -7,9 +7,14 @@ import org.openqa.selenium.WebElement;
 public class LoginPage extends BasicPage {
 	
 	public By loginButton = By.xpath("//*[@class=\"filled\"]/a");
+	public By username = By.name("username");
 	
 	public WebElement getLoginButton() {
 		return this.driver.findElement(loginButton);
+	}
+	
+	public WebElement getUsername() {
+		return this.driver.findElement(username);
 	}
 
 	@Override

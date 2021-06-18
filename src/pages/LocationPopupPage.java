881bb6f49356d8939122637add10ebe495b65c3c
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-18 11:49:30 +0200

    getLocation method

--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -1,5 +1,21 @@
 package pages;
 
-public class LocationPopupPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LocationPopupPage extends BasicPage{
+	
+	public By location = By.xpath("//div[@class=\"location-selector\"]/a");
+	
+	public WebElement getLocation() {
+		return this.driver.findElement(location);
+	}
+
+	@Override
+	public boolean exists(WebDriver driver, By by) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }

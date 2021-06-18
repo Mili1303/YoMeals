9756955c7fc4e508f07e94c09cc912984b6e4930
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-18 13:16:33 +0200

    getPassword uplaod

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -8,6 +8,7 @@ public class LoginPage extends BasicPage {
 	
 	public By loginButton = By.xpath("//*[@class=\"filled\"]/a");
 	public By username = By.name("username");
+	public By password = By.name("password");
 	
 	public WebElement getLoginButton() {
 		return this.driver.findElement(loginButton);
@@ -15,6 +16,10 @@ public class LoginPage extends BasicPage {
 	
 	public WebElement getUsername() {
 		return this.driver.findElement(username);
+	}
+	
+	public WebElement getPassword() {
+		return this.driver.findElement(password);
 	}
 
 	@Override

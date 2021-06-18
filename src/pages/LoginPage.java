80bfa1ef7c6affc2f8a9ca4797cc59b1cf8fca35
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-18 13:14:09 +0200

    getLoginButton method uplaod

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,21 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class LoginPage extends BasicPage {
+	
+	public By loginButton = By.xpath("//*[@class=\"filled\"]/a");
+	
+	public WebElement getLoginButton() {
+		return this.driver.findElement(loginButton);
+	}
+
+	@Override
+	public boolean exists(WebDriver driver, By by) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }

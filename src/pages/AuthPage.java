dae89ba0b693131d9592eee65cc849218909458d
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-22 21:42:36 +0200

    AuthPage upload

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,38 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+	
+	public By account = By.xpath("//div[@class='accounts-link accounts-popup']/ul/li/a");
+	public By myAccount = By.xpath("//div[@class='my-account-dropdown']/ul/li/a");
+	public By logout = By.xpath("//div[@class='my-account-dropdown']/ul/li[2]/a");
+	
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+	
+	public WebElement getAccount() {
+		return this.driver.findElement(account);
+	}
+
+	public WebElement getMyAccount() {
+		return this.driver.findElement(myAccount);
+	}
+
+	public WebElement getLogout() {
+		return this.driver.findElement(logout);
+	}
+
+	public void logout() {
+		this.getAccount().click();
+		waiter.until(ExpectedConditions.visibilityOf(getLogout()));
+		this.getLogout().click();
+	}
 
 }

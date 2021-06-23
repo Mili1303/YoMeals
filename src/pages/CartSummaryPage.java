736a9f16609cd1d765ab5583ebe4d4ab5493d1ab
Milica Stojanovic <stojanovicmilica1303@yahoo.com>
2021-06-24 01:34:39 +0200

    CartSummaryPage upload

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,5 +1,28 @@
 package pages;
 
-public class CartSummaryPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartSummaryPage extends BasicPage {
+	
+	public By clearAll = By.xpath("//*[@onclick=\"clearCartItems()\"]");
+
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+	
+	public WebElement getClearAll() {
+		return this.driver.findElement(clearAll);
+	}
+
+	public void clearCart() {
+		this.getClearAll().click();
+	}
+
+	
+	
 
 }
